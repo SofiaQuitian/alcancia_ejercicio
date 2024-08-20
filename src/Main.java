f8d423c716446335c1ae6f899cf37edaebc92f7e
Sofia Quitian <vquitian@ucundinamarca.edu.co>
2024-08-20 14:11:52 -0500

    ajustes nombre y fecha

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+
 //Sofia Quitian Carrillo//
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//EJERCICIO DE ALCANCIA//
+//20/08/2024//
+
 public class Main {
     public static void main(String[] args) {
 
